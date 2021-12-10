c6e39538f7ff020ca7eeb0e4d62191a614c49ee7
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-12-11 00:16:56 +0900

    02. Rest Template 사용하기 (3)

--- client/src/main/java/com/example/client/service/RestTemplateService.java
+++ client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -1,8 +1,12 @@
 package com.example.client.service;
 
+import com.example.client.dto.Req;
 import com.example.client.dto.UserRequest;
 import com.example.client.dto.UserResponse;
 import java.net.URI;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -23,7 +27,7 @@ public class RestTemplateService {
         .build()
         .toUri();
 
-    System.out.println(uri.toString());
+    System.out.println(uri);
 
     RestTemplate restTemplate = new RestTemplate();
     ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri, UserResponse.class);
@@ -60,5 +64,74 @@ public class RestTemplateService {
     System.out.println(response.getBody());
 
 //    return response.getBody();
+  }
+
+  public UserResponse exchange() {
+    URI uri = UriComponentsBuilder
+        .fromUriString("http://localhost:9090")
+        .path("/api/server/user/{userId}/name/{userName}")
+        .encode()
+        .build()
+        .expand("100", "steve")
+        .toUri();
+
+    System.out.println(uri);
+
+    // http body -> object -> object mapper -> json -> rest template -> http body json
+    UserRequest req = new UserRequest();
+    req.setName("steve");
+    req.setAge(10);
+
+    RequestEntity<UserRequest> requestEntity = RequestEntity
+        .post(uri)
+        .contentType(MediaType.APPLICATION_JSON)
+        .header("x-authorization", "abcd")
+        .header("custom-header", "fffff")
+        .body(req);
+
+    RestTemplate restTemplate = new RestTemplate();
+    ResponseEntity<UserResponse> response = restTemplate.exchange(
+        requestEntity,
+        UserResponse.class
+    );
+    return response.getBody();
+  }
+
+  public Req<UserResponse> genericExchange() {
+    URI uri = UriComponentsBuilder
+        .fromUriString("http://localhost:9090")
+        .path("/api/server/user/{userId}/name/{userName}")
+        .encode()
+        .build()
+        .expand("100", "steve")
+        .toUri();
+
+    System.out.println(uri);
+
+    // http body -> object -> object mapper -> json -> rest template -> http body json
+
+    UserRequest userRequest = new UserRequest();
+    userRequest.setName("steve");
+    userRequest.setAge(10);
+
+    Req req = new Req<UserRequest>();
+    req.setHeader(new Req.Header());
+    req.setResBody(userRequest);
+
+    RequestEntity<Req<UserRequest>> requestEntity = RequestEntity
+        .post(uri)
+        .contentType(MediaType.APPLICATION_JSON)
+        .header("x-authorization", "abcd")
+        .header("custom-header", "fffff")
+        .body(req);
+
+    RestTemplate restTemplate = new RestTemplate();
+    ResponseEntity<Req<UserResponse>> response = restTemplate.exchange(
+        requestEntity,
+        new ParameterizedTypeReference<>() {
+        }
+    );
+
+    return response.getBody();
   }
 }
