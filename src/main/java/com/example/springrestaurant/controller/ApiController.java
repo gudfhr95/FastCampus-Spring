415c46cede4f19ceb3f4c540061c3e4775102aae
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-12-11 23:38:03 +0900

    04. REST API 개발하기 (4)

--- src/main/java/com/example/springrestaurant/controller/ApiController.java
+++ src/main/java/com/example/springrestaurant/controller/ApiController.java
@@ -5,7 +5,9 @@ import com.example.springrestaurant.wishlist.service.WishListService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +36,15 @@ public class ApiController {
   @GetMapping("/all")
   public List<WishListDto> findAll() {
     return wishListService.findAll();
+  }
+
+  @DeleteMapping("/{index}")
+  public void delete(@PathVariable int index) {
+    wishListService.delete(index);
+  }
+
+  @PostMapping("/{index}")
+  public void addVisit(@PathVariable int index) {
+    wishListService.addVisit(index);
   }
 }
